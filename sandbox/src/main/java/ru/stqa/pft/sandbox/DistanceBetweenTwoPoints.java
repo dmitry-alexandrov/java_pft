790bc34dcb980dcb40efb34abc667060860ea5c9
Dmitry <dmitry.alexandrov.u@gmail.com>
2021-12-14 02:28:31 +0300

    Расстояние между двумя точками на плоскости. Выполнены пункты 2 и 3

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenTwoPoints.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+public class DistanceBetweenTwoPoints {
+  public static void main(String[] args) {
+
+    Point p1 = new Point();
+    p1.x = 4;
+    p1.y = 5;
+
+    Point p2 = new Point();
+    p2.x = 2;
+    p2.y = 3;
+    System.out.println("Расстояние между двумя точками = " + distance(p1 , p2));
+  }
+  public  static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
+  }
+
+}
