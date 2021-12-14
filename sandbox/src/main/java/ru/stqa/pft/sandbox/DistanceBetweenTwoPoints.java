061c102bac820b22d88a48da543410a2472d0d9e
Dmitry <dmitry.alexandrov.u@gmail.com>
2021-12-14 22:42:41 +0300

    Вычисление расстояния с помощью метода в классе (попытка 2)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenTwoPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenTwoPoints.java
@@ -3,22 +3,11 @@ package ru.stqa.pft.sandbox;
 public class DistanceBetweenTwoPoints {
   public static void main(String[] args) {
 
-//    Point p1 = new Point();
- //   p1.x = 4;
-//    p1.y = 5;
-
-//    Point p2 = new Point();
-//    p2.x = 2;
-//    p2.y = 3;
-//    System.out.println("Расстояние между двумя точками = " + distance(p1 , p2));
-//  }
-// public  static double distance(Point p1, Point p2) {
-//   return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
+    Point p1 = new Point(4, 5);
 
 
-    Point p = new Point(4, 5, 2, 3);
+    Point p2 = new Point(2, 3);
 
-    System.out.println("Расстояние между двумя точками = " + p.distance());
+    System.out.println("Расстояние между двумя точками = " + p1.distance(p2));
   }
-
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,21 +3,17 @@ package ru.stqa.pft.sandbox;
 import org.testng.annotations.Test;
 
 public class Point {
-  public double x1;
-  public double y1;
-  public double x2;
-  public double y2;
+  public double x;
+  public double y;
 
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
 
-  public Point(double x1, double y1, double x2, double y2) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
   }
 
   @Test
-  public double distance() {
-    return Math.sqrt(Math.pow((this.x1 - this.x2), 2) + Math.pow((this.y1 - this.y2), 2));
+  public double distance(Point p) {
+    return Math.sqrt(Math.pow((this.x - p.x), 2) + Math.pow((this.y - p.y), 2));
   }
 }
