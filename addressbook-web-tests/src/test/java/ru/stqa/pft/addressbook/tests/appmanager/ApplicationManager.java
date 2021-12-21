2aa02b67e25e502c56c660a3d834bf876ed50929
Dmitry <dmitry.alexandrov.u@gmail.com>
2021-12-22 02:31:33 +0300

    Задание №7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/appmanager/ApplicationManager.java
@@ -2,7 +2,9 @@ package ru.stqa.pft.addressbook.tests.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.Browser;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -12,9 +14,20 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
+  private Browser browser;
+
+  public ApplicationManager(Browser browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new ChromeDriver();
+    if (browser.equals(Browser.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(Browser.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(Browser.IE)) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.tests.tests;
 
+import org.openqa.selenium.remote.Browser;
 import ru.stqa.pft.addressbook.tests.appmanager.ApplicationManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(Browser.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
