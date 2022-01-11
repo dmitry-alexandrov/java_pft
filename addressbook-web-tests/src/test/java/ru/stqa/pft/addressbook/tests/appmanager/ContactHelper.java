f4902dd3807b1acd7c81e8b955b814a86f091e4f
Dmitry <dmitry.alexandrov.u@gmail.com>
2022-01-12 02:17:23 +0300

    Задание №9 попытка 2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/appmanager/ContactHelper.java
@@ -47,8 +47,8 @@ public class ContactHelper extends HelperBase {
     wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
   }
 
-  public void initContactModification() {
-    click(By.xpath("//img[@alt='Edit']"));
+  public void initContactModification(int id) {
+    wd.findElement(By.cssSelector("a[href='edit.php?id=" + id + "']")).click();
   }
 
   public void submitContactModification() {
@@ -72,7 +72,7 @@ public class ContactHelper extends HelperBase {
 
   public void modify(ContactData contact) {
     selectContactById(contact.getId());
-    initContactModification();
+    initContactModification(contact.getId());
     fillContactForm(contact, false);
     submitContactModification();
     returnToHomePage();
