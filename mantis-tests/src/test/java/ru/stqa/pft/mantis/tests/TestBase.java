e19151633781f960fddccb3ad9301ff37ba78ba5
Dmitry <dmitry.alexandrov.u@gmail.com>
2022-05-06 11:29:39 +0300

    На проверку

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -3,7 +3,9 @@ package ru.stqa.pft.mantis.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import ru.stqa.pft.addressbook.tests.appmanager.ApplicationManager;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.io.File;
 
 public class TestBase {
 
@@ -12,10 +14,12 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
   }
 
   @AfterSuite
   public void tearDown() throws Exception {
+    app.ftp.restore("config_inc.php.bak", "config_inc.php");
     app.stop();
 
   }
